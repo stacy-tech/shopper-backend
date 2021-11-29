6973a9393b4f934b9eff1ee7c1cf43e9062b60b5
stacy <stacydn23@gmail.com>
2021-11-29 13:27:50 -0500

    structure users controller as well as useer model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+    def create 
+        user = User.new(user_params)
+        if user.save
+            render json: {errors: user.errors.full_messages.to_sentence}, status: :unprocessable_entity
+        end
+
+    end
+
+    private
+
+    def user_params
+        params.permit(:first_name, :last_name, :username, :password)
+    end
+
+
 end
