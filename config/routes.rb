436d62bcad3733ee44a39879c34cd31d404eaaa0
stacy <stacydn23@gmail.com>
2021-11-29 14:46:52 -0500

    structure my sessions controller tw where a user is able to be found by their username which is then authenticated by said user password as well as structure a route for sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :products
+  resources :users, only: [:create]
+  resources :products, only: [:index, :show]
+  resources :sessions, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
