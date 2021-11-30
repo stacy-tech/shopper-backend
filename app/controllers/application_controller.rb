30633e695b61ac19bf890e3526530324bdf21195
stacy <stacydn23@gmail.com>
2021-11-30 18:28:12 -0500

    create a .env file with a jwt secret, as well as encode a token in my application controller, also added a user serializer with a encoded token in my sessions controller for a user to be able to login in with his/her username and password after being authenticated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
 
     def encode_token(user_id)
-        JWT.encode({user_id: user_id})
+        JWT.encode({user_id: user_id}, ENV["JWT_SECRET"])
     end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by_username(params[:username])
         if user && user.authenticate(params[:password])
-            render json: user
+            render json: {user: UserSerializer.new(user), token: encode_token(user.id)}
         else 
             render json: {errors: "Invalid Username and/or Password"}, status: :forbidden
         end
