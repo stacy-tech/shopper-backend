7331f89a515aad6d8243e3171b2405798e497c75
stacy <stacydn23@gmail.com>
2021-12-01 09:18:03 -0500

    create a auto login function with a encode token which takes in the logged in user.id, also structure a get me/ auto login route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,4 +8,8 @@ class SessionsController < ApplicationController
             render json: {errors: "Invalid Username and/or Password"}, status: :forbidden
         end
     end
+
+    def autologin
+        render json: {user: UserSerializer.new(logged_in_user), token: encode_token(logged_in_user.id)}
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
   resources :products, only: [:index, :show]
   resources :sessions, only: [:create]
+  get "/me", to: 'sessions#autologin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
